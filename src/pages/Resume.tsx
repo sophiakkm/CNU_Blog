04eedad63ea01f4d0a21423c905308c4328e445a
kyeongmin <sophia4560@naver.com>
2024-06-01 18:19:01 +0900

    commit

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -7,7 +7,7 @@ const Resume = () => {
       <div style={{ fontWeight: 'bold', fontSize: '24px', marginBottom: '10px' }}>기초프로젝트랩 수강</div>
       <div style={{ fontSize: '18px', marginBottom: '10px' }}>- 2023.03 ~ 2023.06</div>
       <div style={{ fontSize: '18px', marginBottom: '10px' }}>- 자취생을 위한 앱 개발(피그마, 플러터, 파이어베이스 사용)</div>
-      <div style={{ fontSize: '18px', marginBottom: '10px' }}>- 피그마 링크</div>
+      <div style={{ fontSize: '18px', marginBottom: '10px' }}>- 피그마 링크:</div>
       <div style={{ fontSize: '18px', marginBottom: '25px' }}>
         https://www.figma.com/design/o4TJP5SbxuC7VztAkLFseB/%EA%B8%B0%EC%B4%88%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8%EB%9E%A9-%EB%94%94%EC%9E%90%EC%9D%B8?node-id=0-1&t=5qxiLYNXohjK1eWS-1
       </div>
